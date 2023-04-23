e1ee9f4ae1aca2fc9173c48c6c3a8a53f4765997
hoonjoo94 <hoonjoo94@naver.com>
2023-04-23 20:28:36 +0900

    BambooColors 세팅 완료

---
+++ bamboo/Utils/UIHelper.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+enum BambooColors {
+    static let black = UIColor(r: 28, g: 26, b: 29, a: 1)
+    static let pureBlack = UIColor(r: 0, g: 0, b: 0, a: 1)
+    static let white = UIColor(r: 255, g: 255, b: 255, a: 1)
+    static let navy = UIColor(r: 31, g: 31, b: 41, a: 1)
+    static let green = UIColor(r: 62, g: 183, b: 120, a: 1)
+    static let gray = UIColor(r: 138, g: 138, b: 140, a: 1)
+    static let darkGray = UIColor(r: 90, g: 90, b: 90, a: 1)
+    static let kakaoYello = UIColor(r: 254, g: 225, b: 2, a: 1)
+}
