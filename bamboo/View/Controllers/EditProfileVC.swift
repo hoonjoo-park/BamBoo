4475d51840c19d12fd7e5e6a122161e9d1fecc5c
hoonjoo94 <hoonjoo94@naver.com>
2023-05-02 01:28:35 +0900

    EditProfileVC SubView들 SnapKit으로 레이아웃 잡기 완료

--- bamboo/View/Controllers/EditProfileVC.swift
+++ bamboo/View/Controllers/EditProfileVC.swift
@@ -2,10 +2,12 @@ import UIKit
 import RxSwift
 import RxCocoa
 import SnapKit
+import Kingfisher
 
 class EditProfileVC: UIViewController {
     let profileImageButton = UIButton()
     let profileImageTitleLabel = BambooLabel(fontSize: 16, weight: .medium, color: BambooColors.gray)
+    let profileImageView = UIImageView()
     let profileImageDescriptionLabel = BambooLabel(fontSize: 12, weight: .medium, color: BambooColors.gray)
     let usernameTitleLabel = BambooLabel(fontSize: 16, weight: .medium, color: BambooColors.gray)
     let usernameInput = UIInputView(frame: .zero, inputViewStyle: .keyboard)
@@ -28,18 +30,14 @@ class EditProfileVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let backButton = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
-        
-        view.backgroundColor = BambooColors.black
-        navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
-        navigationController?.navigationBar.frame.origin.y = view.safeAreaInsets.top
+        configureViewController()
         configureSubViews()
+        bindUserVM()
     }
     
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        let bounds = self.navigationController!.navigationBar.bounds
         
         title = "프로필 수정"
         navigationController?.isNavigationBarHidden = false
@@ -55,8 +53,17 @@ class EditProfileVC: UIViewController {
     }
     
     
+    private func configureViewController() {
+        let backButton = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        
+        view.backgroundColor = BambooColors.black
+        navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
+        navigationController?.navigationBar.frame.origin.y = view.safeAreaInsets.top
+    }
+    
+    
     private func configureSubViews() {
-        [profileImageTitleLabel, profileImageButton, profileImageDescriptionLabel, usernameTitleLabel, usernameInput, saveButton].forEach {
+        [profileImageTitleLabel, profileImageView, profileImageDescriptionLabel, usernameTitleLabel, usernameInput, saveButton].forEach {
             view.addSubview($0)
         }
         
@@ -67,10 +74,49 @@ class EditProfileVC: UIViewController {
         saveButton.backgroundColor = BambooColors.green
         saveButton.layer.cornerRadius = 8
         
+        profileImageTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.leading.equalToSuperview().inset(25)
+        }
+        
+        profileImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.width.height.equalTo(75)
+            make.top.equalTo(profileImageTitleLabel.snp.bottom).offset(45)
+        }
+        
+        profileImageDescriptionLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(profileImageView.snp.bottom).offset(20)
+        }
+        
+        usernameTitleLabel.snp.makeConstraints { make in
+            make.top.equalTo(profileImageDescriptionLabel.snp.bottom).offset(45)
+            make.leading.equalToSuperview().inset(25)
+        }
+        
+        usernameInput.snp.makeConstraints { make in
+            make.top.equalTo(usernameTitleLabel.snp.bottom).offset(25)
+            make.height.equalTo(50)
+            make.horizontalEdges.equalToSuperview().inset(25)
+        }
+        
         saveButton.snp.makeConstraints { make in
             make.horizontalEdges.equalToSuperview().inset(25)
             make.height.equalTo(45)
             make.bottom.equalTo(view.safeAreaLayoutGuide).inset(35)
         }
+    }
+    
+    
+    private func bindUserVM() {
+        userVM.user.subscribe(onNext: { [weak self] user in
+            if let user = user {
+                if let profileImage = user.profile.profileImage,
+                   let profileImageUrl = URL(string: profileImage) {
+                    self?.profileImageView.kf.setImage(with: profileImageUrl)
+                }
+            }
+        }).disposed(by: disposeBag)
     }
 }
