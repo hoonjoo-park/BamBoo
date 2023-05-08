a77af5b88d1524d8b816be729cb4e4c7963f1f35
hoonjoo94 <hoonjoo94@naver.com>
2023-05-08 23:19:27 +0900

    LocationVC SaveLocationButton 구현

--- bamboo/View/Controllers/LocationVC.swift
+++ bamboo/View/Controllers/LocationVC.swift
@@ -2,9 +2,20 @@ import UIKit
 
 class LocationVC: BottomSheetVC {
     var fromVC: String!
+    
+    lazy var saveLocationButton: LabelButton = {
+        let button = LabelButton(fontSize: 14, weight: .semibold, color: BambooColors.white)
+        button.buttonLabel.text = "위치 선택 완료"
+        button.layer.cornerRadius = 8
+        button.backgroundColor = BambooColors.green
+        
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureSubViews()
     }
     
     
@@ -16,5 +27,25 @@ class LocationVC: BottomSheetVC {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configureSubViews() {
+        [saveLocationButton].forEach {
+            containerView.addSubview($0)
+        }
+        
+        saveLocationButton.addTarget(self, action: #selector(handleTapSaveLocationButton), for: .touchUpInside)
+        saveLocationButton.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(containerView).inset(25)
+            make.height.equalTo(45)
+            make.bottom.equalTo(containerView.snp.bottom).inset(35)
+        }
+    }
+    
+    
+    @objc private func handleTapSaveLocationButton() {
+        // TODO: 선택한 위치 저장 및 ViewModel 업데이트 하는 로직 구현 필요
+        print("Save Location Button Tapped!")
     }
 }
