46f13b9a302d7fa2fedda9a0103206fd81b29a4a
hoonjoo94 <hoonjoo94@naver.com>
2023-05-21 20:11:58 +0900

    ArticleListCollectionView 보일러플레이트 코드 작성

---
+++ bamboo/View/CollectionViews/ArticleListCollectionView.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ArticleListCollectionView: UICollectionView {
+
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: frame, collectionViewLayout: layout)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureCollectionView() {
+        backgroundColor = BambooColors.lightNavy
+    }
+    
+}
