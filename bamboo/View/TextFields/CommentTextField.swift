ec658cc3d12cd565f7c65009b7fb1fb4e42ea63a
hoonjoo94 <hoonjoo94@naver.com>
2023-06-10 16:42:39 +0900

    TextField 관련 View 생성

---
+++ bamboo/View/TextFields/CommentTextField.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class CommentTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
