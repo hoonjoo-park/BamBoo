60b1582c68a7cf15bcc50f75ea2488b00d268d6b
hoonjoo94 <hoonjoo94@naver.com>
2023-11-30 20:45:53 +0900

    separator 제거 및 selectionStyle 지정

--- bamboo/View/Cells/ChatRoomTableViewCell.swift
+++ bamboo/View/Cells/ChatRoomTableViewCell.swift
@@ -14,8 +14,6 @@ class ChatRoomTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        self.selectionStyle = .none
-        
         configureUI()
     }
     
